5f47e8fffd7b736cc41812c4acc2362e03600ee5
nimurungipreciouswinnie <nimurungipreciouswinnie@gmail.com>
2026-03-30 04:58:51 -0700

    I have registered the user model in admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ('username', 'email', 'role', 'is_staff')
+    list_filter = ('role',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
