864a990b22d8c78283e3cfc90f3bf53f8753c1fc
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-03-30 15:26:12 +0300

    registered models for internships in admin.py

--- backend/internships/admin.py
+++ backend/internships/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Organization, Application, Placement
 
-# Register your models here.
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'contact_email')
+    search_fields = ('name',)
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('student', 'organization', 'status', 'applied_at')
+    list_filter = ('status',)
+    search_fields = ('student__username', 'organization__name')
+
+
+@admin.register(Placement)
+class PlacementAdmin(admin.ModelAdmin):
+    list_display = (
+        'student',
+        'organization',
+        'workplace_supervisor',
+        'academic_supervisor'
+    )
+
+    search_fields = ('student__username',)
