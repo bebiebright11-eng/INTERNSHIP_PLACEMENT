cea3c843e6e51b6074400af6400f259c7f1de58a
nimurungipreciouswinnie <nimurungipreciouswinnie@gmail.com>
2026-04-07 11:30:45 -0700

    i have added the email and phone number in the user models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -11,6 +11,8 @@ class User(AbstractUser):
     )
 
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-
+    phone_number = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(max_length=40, blank=True , null=True)
+    
     def __str__(self):
         return f"{self.username} ({self.role})"
