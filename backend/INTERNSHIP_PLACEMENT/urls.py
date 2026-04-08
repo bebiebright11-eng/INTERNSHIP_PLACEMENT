8ad323a1c7fca57ea0d2d5bd63bb97a3bc4d2409
kizzamathias20-collab <kizzamathias20@gmail.com>
2026-04-08 12:35:02 +0300

    finishing the surverpage

--- backend/INTERNSHIP_PLACEMENT/urls.py
+++ backend/INTERNSHIP_PLACEMENT/urls.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
+
+def home(request):
+    return HttpResponse("welcome to the internship Placement API!")
 
 admin.site.site_header = "Internship Placement System"
 admin.site.site_title = "Internship Admin"
 admin.site.index_title = "Welcome to Internship Dashboard"
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
 
     path('api/accounts/', include('accounts.urls')),
