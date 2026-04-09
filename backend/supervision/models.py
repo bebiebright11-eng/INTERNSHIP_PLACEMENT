ed3b95448e776b57cc5810381e9e0563756e5e56
bebiebright11-eng <bebiebright11@gmail.com>
2026-04-09 15:44:20 +0300

    added supervisor_type, and disallowe giving two supervisors to one placement

--- backend/supervision/models.py
+++ backend/supervision/models.py
@@ -39,11 +39,20 @@ class Evaluation(models.Model):
     placement = models.ForeignKey(Placement, on_delete=models.CASCADE)
 
     supervisor = models.ForeignKey(User, on_delete=models.CASCADE)
+    supervisor_type = models.CharField(
+        max_length=20,
+        choices=(
+            ('workplace', 'Workplace Supervisor'),
+            ('academic', 'Academic Supervisor'),
+        )
+    )
 
     score = models.IntegerField()
     comments = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ['placement', 'supervisor']
 
     def __str__(self):
         return f"Evaluation - {self.placement}"
