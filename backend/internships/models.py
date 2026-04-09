9583ec66333455af2807c7f46f0e9c2aff8b8a88
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-04-09 16:17:04 +0300

    added status for placements

--- backend/internships/models.py
+++ backend/internships/models.py
@@ -55,6 +55,11 @@ class Placement(models.Model):
 
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=(('active', 'Active'), ('completed', 'Completed')),
+        default='active'
+    )
 
     def __str__(self):
         return f"{self.student} Placement"
