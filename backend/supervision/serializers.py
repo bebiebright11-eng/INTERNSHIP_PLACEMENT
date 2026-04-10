022d106bb15d4e3e69e28421cad426c713e356c7
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-04-10 16:32:17 +0300

    modified evaluation serializer to calculate final score automatically

--- backend/supervision/serializers.py
+++ backend/supervision/serializers.py
@@ -21,3 +21,45 @@ class CriteriaScoreSerializer(serializers.ModelSerializer):
         fields = ['id', 'criteria', 'score']
 
 
+class EvaluationSerializer(serializers.ModelSerializer):
+    criteria_scores = CriteriaScoreSerializer(many=True)
+
+    class Meta:
+        model = Evaluation
+        fields = [
+            'id',
+            'placement',
+            'supervisor',
+            'supervisor_type',
+            'score',
+            'comments',
+            'final_grade',
+            'is_final',
+            'criteria_scores'
+        ]
+
+    def create(self, validated_data):
+        criteria_data = validated_data.pop('criteria_scores')
+        evaluation = Evaluation.objects.create(**validated_data)
+
+        total = 0
+
+        for item in criteria_data:
+            CriteriaScore.objects.create(
+                evaluation=evaluation,
+                criteria=item['criteria'],
+                score=item['score']
+            )
+            total += item['score']
+
+        # ✅ auto total score
+        evaluation.score = total
+
+        # ✅ if academic → final
+        if evaluation.supervisor_type == 'academic':
+            evaluation.final_grade = total
+            evaluation.is_final = True
+
+        evaluation.save()
+
+        return evaluation
