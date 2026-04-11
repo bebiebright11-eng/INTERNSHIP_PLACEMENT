9d552d4ef7cb9ddb7a3c1050317895b0b3c5cc14
bebiebright11-eng <bebiebright11@gmail.com>
2026-04-11 20:27:35 +0300

    added authentication endpoints in urls.py of accounts app)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users',UserViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
+    path('', include(router.urls)), 
+    path('login/', MyTokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
