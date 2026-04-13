e5cba7640c638e9e46e134463002c7a699307dd8
kizzamathias20-collab <kizzamathias20@gmail.com>
2026-04-13 15:53:33 +0300

    modify weekly log veiwset so that each student only their logs

--- backend/supervision/views.py
+++ backend/supervision/views.py
@@ -8,11 +8,23 @@ from .serializers import (
     CriteriaScoreSerializer
 )
 
+from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import IsStudentOrSupervisor, IsEvaluator
+
+
 class WeeklyLogViewSet(viewsets.ModelViewSet):
     queryset = WeeklyLog.objects.all()
     serializer_class = WeeklyLogSerializer
+    permission_classes = [IsAuthenticated, IsStudentOrSupervisor]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == 'student':
+            return WeeklyLog.objects.filter(placement__student=user)
+
+        return WeeklyLog.objects.all()
+
+    
 
 
-class EvaluationViewSet(viewsets.ModelViewSet):
-    queryset = Evaluation.objects.all()
-    serializer_class = EvaluationSerializer
