2c227141d58cd692863fa2d22659a7d4b73d7016
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-04-16 14:49:52 +0300

    added function in the workplace dashboard

---
+++ frontend/src/pages/WorkplaceDashboard.jsx
@@ -0,0 +1,9 @@
+function WorkplaceDashboard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Workplace Supervisor Dashboard</h1>
+    </div>
+  );
+}
+
+export default WorkplaceDashboard;
