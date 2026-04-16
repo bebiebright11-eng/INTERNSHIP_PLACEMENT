d226dcd8de198f73da81fc9486852dc37bc08cd1
nimurungipreciouswinnie <nimurungipreciouswinnie@gmail.com>
2026-04-16 03:46:24 -0700

    i added the react router setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1>App Started</h1>
+  <BrowserRouter>
+    <h1>Routing Started</h1>
+  </BrowserRouter>
 );
