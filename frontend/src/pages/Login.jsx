4d6817ab0658827f989d4bf9f24b67c4951d6b15
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-04-16 18:30:43 +0300

    import React hooks and dependencies

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../api";
+
 function Login() {
   return (
     <div style={{ padding: "40px" }}>
-      <h2>Login</h2>
+      <h2>LOGIN</h2>
     </div>
   );
 }
