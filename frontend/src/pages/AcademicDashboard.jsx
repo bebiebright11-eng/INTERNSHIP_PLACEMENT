254031547d3f2881cff060f40760b8e7ab987905
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-04-16 15:12:43 +0300

    initialised the admin dashboard

---
+++ frontend/src/pages/AcademicDashboard.jsx
@@ -0,0 +1,9 @@
+function AcademicDashboard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Academic Supervisor Dashboard</h1>
+    </div>
+  );
+}
+
+export default AcademicDashboard;
