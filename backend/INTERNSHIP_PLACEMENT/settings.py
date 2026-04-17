e2eeaf4c67095131f1d0f5316376dcdbb8c1e68f
bebiebright11-eng <bebiebright11@gmail.com>
2026-04-17 12:58:08 +0300

    resolved cors policy error for a student to be able to login to the system

--- backend/INTERNSHIP_PLACEMENT/settings.py
+++ backend/INTERNSHIP_PLACEMENT/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -142,6 +143,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'accounts.User'  # Replace 'accounts' with your actual app name
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 CORS_ALLOW_ALL_ORIGINS = True 
 
 REST_FRAMEWORK = {
