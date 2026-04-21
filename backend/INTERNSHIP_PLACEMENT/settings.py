8503ac98d314a4217adcc85e8248285d5347cf2b
bebiebright11-eng <bebiebright11@gmail.com>
2026-04-21 13:19:26 +0300

    allowed host 5174 in settings

--- backend/INTERNSHIP_PLACEMENT/settings.py
+++ backend/INTERNSHIP_PLACEMENT/settings.py
@@ -147,6 +147,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+]
 CORS_ALLOW_ALL_ORIGINS = True 
 
 REST_FRAMEWORK = {
