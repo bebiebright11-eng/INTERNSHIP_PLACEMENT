7c5ca34c2e472875f50b1c8e620762b4832e67d0
bebiebright11-eng <bebiebright11@gmail.com>
2026-04-22 16:14:26 +0300

    added names under application and placement to replace the IDs

--- backend/internships/serializers.py
+++ backend/internships/serializers.py
@@ -8,11 +8,21 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    organization_name = serializers.CharField(source='organization.name', read_only=True)
+
+    #  ADD: student name
+    student_name = serializers.CharField(source='student.username', read_only=True)
     class Meta:
         model = Application
         fields = '__all__'
+                #Added for apply reasons
+        read_only_fields = ['student']
 
 class PlacementSerializer(serializers.ModelSerializer):
+    organization_name = serializers.CharField(source='organization.name', read_only=True)
+
+    #  ADD: student name
+    student_name = serializers.CharField(source='student.username', read_only=True)
     class Meta:
         model = Placement
         fields = '__all__'
