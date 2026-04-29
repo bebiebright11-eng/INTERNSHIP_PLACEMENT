a80a5f5824bdcfcef21dd00b2b1a12059ad185d0
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-04-29 23:02:57 +0300

    activated models in criteria

--- backend/supervision/models.py
+++ backend/supervision/models.py
@@ -62,6 +62,7 @@ class Evaluation(models.Model):
 class EvaluationCriteria(models.Model):
     name = models.CharField(max_length=100)
     max_score = models.IntegerField()
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
