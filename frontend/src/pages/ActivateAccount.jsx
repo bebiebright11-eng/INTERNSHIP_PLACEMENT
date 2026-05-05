b3112d3df9854aca701bad86c9de9ddd4662097a
bebiebright11-eng <bebiebright11@gmail.com>
2026-05-05 23:35:00 +0300

    added activation form on its dashboard

--- frontend/src/pages/ActivateAccount.jsx
+++ frontend/src/pages/ActivateAccount.jsx
@@ -1,1 +1,38 @@
-s
+import { useState } from "react";
+
+function ActivateAccount() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Activate Account</h2>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Enter Registration Number or Email"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Create Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <br /><br />
+
+        <button type="submit">Activate</button>
+      </form>
+    </div>
+  );
+}
+
+export default ActivateAccount;
