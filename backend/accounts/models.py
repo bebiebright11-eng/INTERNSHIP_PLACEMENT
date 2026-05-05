6260deb6484427ed4e1549a66561fa8df4a02ace
bebiebright11-eng <bebiebright11@gmail.com>
2026-05-05 22:49:49 +0300

    allowed public access of account activation by use of  AllowAny

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     phone_number = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=40, blank=True , null=True)
+    is_activated = models.BooleanField(default=False)
 
     organization = models.ForeignKey(
         'internships.Organization',
