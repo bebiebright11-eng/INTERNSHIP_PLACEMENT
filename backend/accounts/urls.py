fa0b7f7cc63d6ce4692b113c2995abe1cb66075e
bebiebright11-eng <bebiebright11@gmail.com>
2026-05-05 22:33:58 +0300

    added account activation logic

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, MyTokenObtainPairView
+from .views import UserViewSet, MyTokenObtainPairView,activate_account
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users',UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)), 
+    path('', include(router.urls)),
+    path('activate/', activate_account), 
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -10,3 +10,33 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@api_view(['POST'])
+def activate_account(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    # 1. Check user exists
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
+
+    # 2. Check if already activated
+    if user.is_activated:
+        return Response({"error": "Account already activated"}, status=400)
+
+    # 3. Set password
+    user.set_password(password)
+    user.is_activated = True
+    user.save()
+
+    return Response({"message": "Account activated successfully"})
