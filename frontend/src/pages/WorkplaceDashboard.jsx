dfcd43f9c5ff934cacc31a147f7ddd8091cf1f7e
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-06-09 08:50:30 +0300

    added import footer for workplace dashboard

--- frontend/src/pages/WorkplaceDashboard.jsx
+++ frontend/src/pages/WorkplaceDashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import API from "../api";
+import Footer from "../components/Footer";
 
 function WorkplaceDashboard() {
   const navigate = useNavigate();
