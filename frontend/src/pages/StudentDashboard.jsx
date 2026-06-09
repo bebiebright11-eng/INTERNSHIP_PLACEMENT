b68647c47d503364ee454f2cc81d35c5bc556f8a
NANSUBUGA SWABAHAT <washibatswaby@gmail.com>
2026-06-09 08:49:12 +0300

    added import footer for student dashboard

--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import API from "../api";
+import Footer from "../components/Footer";
 
 function StudentDashboard() {
   // Adding a menu
