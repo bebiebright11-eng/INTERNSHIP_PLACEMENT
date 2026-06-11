e26eb4fa1f1a092a8c319f7f53bfa073719af451
bebiebright11-eng <bebiebright11@gmail.com>
2026-06-11 06:58:29 +0300

    Implement admin placement management with edit and delete features

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,9 +3,23 @@ from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['id', 'username','organization', 'email', 'role']
+        fields = [
+            'id',
+            'username',
+            'organization',
+            'email',
+            'role',
+            'first_name',
+            'last_name',
+            'full_name',
+        ]
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip()
+        
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
